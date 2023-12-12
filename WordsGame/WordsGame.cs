5d9e52b05e364ac9cb488c16a5c6b9568f919673
Patrick Bucher <patrick.bucher@sluz.ch>
2023-12-12 11:44:20 +0100

    TODO tasks

--- WordsGame/WordsGame.cs
+++ WordsGame/WordsGame.cs
@@ -4,11 +4,14 @@ public class WordsGame : IWordsGame
 {
     public string Start(string word)
     {
+        // TODO: store the original word for later comparison
+        // TODO: return a scrambled version of the word
         return "";
     }
 
     public int Grade(string solution)
     {
+        // TODO: award one point per letter if the solution is the original word
         return 0;
     }
 }
